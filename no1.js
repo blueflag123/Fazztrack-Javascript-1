0c1ee2ee77214066b3b364ec3c9ce9588a1c262d
Dio Fitriadi <102437769+blueflag123@users.noreply.github.com>
2022-05-27 14:10:51 +0700

    Create no1.js

---
+++ no1.js
@@ -0,0 +1,37 @@
+const nama = "Muhammad Dio Fitriadi";
+let umur = 23;
+let hobi = ["main games", "coding", "berenang"];
+const isMarried = false;
+const daftarSekolah = [
+    { 
+        namaSekolah: "SDN53Kuranji", 
+        tahunMasuk: "2005", 
+        tahunKeluar: "2010", 
+        jurusan: null
+    },
+    { 
+        namaSekolah : "SMP4Padang", 
+        tahunMasuk: "2010", 
+        tahunKeluar: "2013",
+        jurusan: null
+    },
+    { 
+        namaSekolah : "MAN2Padang", 
+        tahunMasuk: "2013", 
+        tahunKeluar: "2016",
+        jurusan: "IPS"
+    },
+    { 
+        namaSekolah : "universitasUdayana", 
+        tahunMasuk: "2017", 
+        tahunKeluar: "2021" }
+];
+let kemampuan = [
+    { skills: "HTML", level: "advanced" },
+    { skills: "CSS", level: "advanced" },
+    { skills: "javascript", level: "beginner" },
+    { skills: "microsoftOffice", level: "advanced" },
+    { skills: "english", level: "beginner" },
+    { skills: "japanese", level: "beginner" }
+];
+const tertarikDenganCoding = true;
