1df1209b4027d405740df440fecfeb8f3a446dc5
Dio Fitriadi <102437769+blueflag123@users.noreply.github.com>
2022-05-29 16:02:25 +0700

    Update no2.js

--- no2.js
+++ no2.js
@@ -1,9 +1,9 @@
 const mtk = 90;
-const IPA = 86;
-const bahasaIndonesia = 82;
-const bahasaInggris = 76;
+const IPA = 85;
+const bahasaIndonesia = 86;
+const bahasaInggris = 75;
 let hasil = mtk + IPA + bahasaIndonesia + bahasaInggris;
-let nilai = hasil/4 ;
+let nilai = hasil/4;
 let nilaiRata;
     if (nilai >= 90 && nilai <= 100) {
         nilaiRata = "A";
@@ -16,10 +16,8 @@ let nilaiRata;
     } else if (nilai >= 0 && nilai <= 59) {
         nilaiRata = "E";
     } else {
-        console.log("input yang anda masukkan salah");
+        nilaiRata = "input salah";
     }
-console.log(`Rata-rata adalah : ${nilai}`);
-console.log(`Grade : ${nilaiRata}`);
 
-
-
+console.log(`Rata-rata adalah : ${nilai}
+Grade : ${nilaiRata}`);
