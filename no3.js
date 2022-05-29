d2b04ea1408cdd63a3f9a8ae7f3f3aa3ed905293
Dio Fitriadi <102437769+blueflag123@users.noreply.github.com>
2022-05-29 16:03:41 +0700

    Update no3.js

--- no3.js
+++ no3.js
@@ -1,9 +1,8 @@
 printSegitiga = (num) =>  {
     let string = "";
     for (let i = 1; i <= num; i++) {
-      for (let j = 1; j <= num - i + 1 ; j++) {
+      for (let j = 1; j <= num - i + 1; j++) {
         string = `${string} ${j} `;
-      
       }
         string = `${string}
 `;
