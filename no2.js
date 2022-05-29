2a664f369d064c26c9c3bfdfa4d6f63cb8a4b1bd
Dio Fitriadi <102437769+blueflag123@users.noreply.github.com>
2022-05-29 20:31:17 +0700

    Update no2.js

--- no2.js
+++ no2.js
@@ -4,20 +4,24 @@ const bahasaIndonesia = 86;
 const bahasaInggris = 75;
 let hasil = mtk + IPA + bahasaIndonesia + bahasaInggris;
 let nilai = hasil/4;
-let nilaiRata;
+let grade;
     if (nilai >= 90 && nilai <= 100) {
-        nilaiRata = "A";
+        grade = "A";
     } else if (nilai >= 80 && nilai <= 89) {
-        nilaiRata = "B";
+        grade = "B";
     } else if (nilai >= 70 && nilai <= 79) {
-        nilaiRata = "C";
+        grade = "C";
     } else if (nilai >= 60 && nilai <= 69) {
-        nilaiRata = "D";
+        grade = "D";
     } else if (nilai >= 0 && nilai <= 59) {
-        nilaiRata = "E";
+        grade = "E";
     } else {
-        nilaiRata = "input salah";
+        grade = "input salah";
     }
 
 console.log(`Rata-rata adalah : ${nilai}
-Grade : ${nilaiRata}`);
+Grade : ${grade}`);
+
+
+
+
